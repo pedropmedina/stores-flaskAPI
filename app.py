6a74c45b0f250f202ed16f2fb3ecce4a59d69a0d
pedropmedina <pedrodelilia@gmail.com>
2019-02-07 11:48:02 -0500

    Fix store/<int:name> to store/<string:name>

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ jwt = JWT(app, authenticate, identity)
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-api.add_resource(Store, '/store/<int:name>')
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
